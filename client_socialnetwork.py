640222961c44e7b2943e504db361649a959af123
Gabriel Nascimento da Silva <gabriel.silva@dbserver.com.br>
2023-10-08 18:55:34 -0300

    client_socialnetwork atualizado

--- client_socialnetwork.py
+++ client_socialnetwork.py
@@ -1,7 +1,11 @@
 class Client_Socialnetwork:
 
-    def __init__(self, Client, Socialnetwork):
-        self.client = Client
-        self.socialnetwork = Socialnetwork
+    def __init__(self, client, socialnetwork):
+        self.obj_client = client
+        self.obj_socialnetwork = socialnetwork
 
-    
+    def get_obj_client(self):
+        return self.obj_client
+
+    def get_obj_socialnetwork(self):
+        return self.obj_socialnetwork
