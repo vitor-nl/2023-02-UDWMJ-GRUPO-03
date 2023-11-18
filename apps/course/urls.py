45f6f8b76a86040165a6d8085faf3cae3569a702
Thiago Rossoni da Silva Dias <56520496+TRZZZZ@users.noreply.github.com>
2023-11-18 18:34:37 -0300

    Update urls.py

--- apps/course/urls.py
+++ apps/course/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 app_name = 'course'
 
 router = routers.DefaultRouter()
-router.register('cursos', views.CoursesViewSet, basename='cursos')
+router.register('courses', views.CoursesViewSet, basename='courses')
 
 urlpatterns = [
     path('', include(router.urls) )
